1a9244484710439ebf2e41419c38970e1096bf81
stachu <stivekowalski@gmail.com>
2018-11-10 18:20:49 +0100

    Add POJO User class

---
+++ src/main/java/com/user/domain/User.java
@@ -0,0 +1,53 @@
+package com.user.domain;
+
+public class User implements Comparable<User> {
+
+    public User(Long id, String firstName, String lastName, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public int compareTo(User userToCompare) {
+    	return 0;
+    }
+}
