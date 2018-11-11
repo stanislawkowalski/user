eca1937cfec1071bdfa471f2b9dd42d5fcd90eb9
stachu <stivekowalski@gmail.com>
2018-11-11 10:28:10 +0100

    Add ignoring white signs and case when lastName and firstName are comparing

--- src/main/java/com/user/service/UserComparator.java
+++ src/main/java/com/user/service/UserComparator.java
@@ -8,15 +8,20 @@ public class UserComparator implements Comparator<User>{
 
 	@Override
 	public int compare(User userFirst, User userSecond) {
-		int compareLastName = userFirst.getLastName().compareTo(userSecond.getLastName());
+		int compareLastName = prepareStringToCompare(userFirst.getLastName())
+				.compareTo(prepareStringToCompare(userSecond.getLastName()));
         if (compareLastName != 0) {
             return compareLastName;
         }
-        int compareFirstName = userFirst.getFirstName().compareTo(userSecond.getFirstName());
+        int compareFirstName = prepareStringToCompare(userFirst.getFirstName())
+        		.compareTo(prepareStringToCompare(userSecond.getFirstName()));
         if (compareFirstName != 0) {
             return compareFirstName;
         }
         return userFirst.getId().compareTo(userSecond.getId());
 	}
 
+	private String prepareStringToCompare(String toPrepare) {
+		return toPrepare.trim().toLowerCase();
+	}
 }
--- src/test/java/com/user/service/UserComparatorTest.java
+++ src/test/java/com/user/service/UserComparatorTest.java
@@ -24,44 +24,60 @@ public class UserComparatorTest {
 
     @Test
     public void testCompare_isAlphabeticallyBefore() {
-        int compareResult = userComparator.compare(testUser, new User(1L, "Artur", "Nowak", TEST_ADDRESS));
+        int compareResult = compareTestUser(new User(1L, "Artur", "Nowak", TEST_ADDRESS));
         assertTrue(isNegative(compareResult));
     }
 
     @Test
     public void testCompare_isAlphabeticallyAfter() {
-        int compareResult = userComparator.compare(testUser, new User(3L, "Zbigniew", "Kowal", TEST_ADDRESS)); 
+        int compareResult = compareTestUser(new User(3L, "Zbigniew", "Kowal", TEST_ADDRESS)); 
         assertTrue(isPositive(compareResult));
     }
 
     @Test
     public void testCompare_isByFirstNameAlphabeticallyBefore() {
-        int compareResult = userComparator.compare(testUser, new User(1L, "Zbigniew", TEST_LAST_NAME, TEST_ADDRESS));
+        int compareResult = compareTestUser(new User(1L, "Zbigniew", TEST_LAST_NAME, TEST_ADDRESS));
         assertTrue(isNegative(compareResult));
     }
 
     @Test
     public void testCompare_isByFirstNameAlphabeticallyAfter() {
-        int compareResult = userComparator.compare(testUser, new User(3L, "Artur", TEST_LAST_NAME, TEST_ADDRESS));
+        int compareResult = compareTestUser(new User(3L, "Artur", TEST_LAST_NAME, TEST_ADDRESS));
         assertTrue(isPositive(compareResult));
     }
 
     @Test
     public void testCompare_isByIdAlphabeticallyBefore() {
-        int compareResult = userComparator.compare(testUser, new User(3L, TEST_FIRST_NAME, TEST_LAST_NAME, TEST_ADDRESS));
+        int compareResult = compareTestUser(new User(3L, TEST_FIRST_NAME, TEST_LAST_NAME, TEST_ADDRESS));
         assertTrue(isNegative(compareResult));
     }
 
     @Test
     public void testCompare_isByIdAlphabeticallyAfter() {
-        int compareResult = userComparator.compare(testUser, new User(1L, TEST_FIRST_NAME, TEST_LAST_NAME, TEST_ADDRESS));
+        int compareResult = compareTestUser(new User(1L, TEST_FIRST_NAME, TEST_LAST_NAME, TEST_ADDRESS));
         assertTrue(isPositive(compareResult));
     }
 
     @Test
     public void testCompare_isAlphabeticallyEqual() {
-        int compareResult = userComparator.compare(testUser, new User(2L, TEST_FIRST_NAME, TEST_LAST_NAME, "Szczecin"));
+        int compareResult = compareTestUser(new User(2L, TEST_FIRST_NAME, TEST_LAST_NAME, "Szczecin"));
         assertTrue(isZero(compareResult));
+    }
+    
+    @Test
+    public void testCompare_isWhiteSignBeforeLastName() {
+    	int compareResult = compareTestUser(new User(1L, "Artur", " Nowak", TEST_ADDRESS));
+    	assertTrue(isNegative(compareResult));
+    }
+    
+    @Test
+    public void testCompare_isLastNameStartFromLowerCase() {
+    	int compareResult = compareTestUser(new User(1L, "Artur", "kowalski", TEST_ADDRESS));
+    	assertTrue(isPositive(compareResult));
+    }
+    
+    private int compareTestUser(User userToCompare) {
+    	return userComparator.compare(testUser, userToCompare);
     }
 
     private boolean isPositive(int compareResult) {
